9536252ba5fa09972a5b30cbf0e1c8b3ce1915e3
Zain Khan <za1nkhan@icloud.com>
2023-06-01 10:02:58 -0400

    demo run

---
+++ Test Suites/tim news tests.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>tim news tests</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>04b2e431-ce08-4ed4-8b61-4752744ae931</testSuiteGuid>
+</TestSuiteEntity>
